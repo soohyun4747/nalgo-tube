8dd077c5c954844623f9ed10ee13f161f3fd4980
soohyun4747 <soohyun4747@gmail.com>
2025-11-25 14:35:44 +0900

    launch page added

---
+++ android/app/src/main/java/com/shjung/nalgotube/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shjung.nalgotube;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shjung.nalgotube',
+  appName: 'NalgoTube',
+  webDir: 'out'
+};
+
+export default config;
